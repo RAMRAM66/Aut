39fe62d4deb36a36fc199e102ea763ff84a1a857
RAMRAM66 <ram-asou@mail.ru>
2020-11-19 22:15:16 +0300

    Add project files.

---
+++ Aut/Migrations/202011191843555_DataMigration1.cs
@@ -0,0 +1,28 @@
+namespace Aut.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataMigration1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Rooms",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        RoomCount = c.Int(nullable: false),
+                        BedCount = c.Int(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Rooms");
+        }
+    }
+}
